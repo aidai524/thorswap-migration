26653d3c47db7dce6fbcb7880d0d623cef3febb8
Emily <emily@ref.finance>
2026-01-27 21:21:39 +0800

    change another rpc.

--- config/chains.ts
+++ config/chains.ts
@@ -2,15 +2,15 @@ import { mainnet, base, type Chain } from "wagmi/chains";
 
 // Custom Base chain with specified RPC URL
 const baseChain: Chain = {
-  ...base,
-  rpcUrls: {
-    default: {
-      http: ["https://base.drpc.org"]
-    },
-    public: {
-      http: ["https://base.drpc.org"]
-    }
-  }
+  ...base
+  // rpcUrls: {
+  //   default: {
+  //     http: ["https://base.drpc.org"]
+  //   },
+  //   public: {
+  //     http: ["https://base.drpc.org"]
+  //   }
+  // }
 };
 
 export default [mainnet, baseChain] as Chain[];
